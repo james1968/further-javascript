0d6b399c356b661c60f56138ab9cbd99439cc238
Tomy8s <tomy8s@hotmail.com>
2016-11-07 15:44:37 +0000

    Added test for Note.allNotes()

--- spec/note-list-model-tests.js
+++ spec/note-list-model-tests.js
@@ -1,11 +1,21 @@
 function testsForNoteListModel() {
     var list = new NoteList();
-    var text = "Javascript is a wonderful language and the easiest to learn and ;everybody loves it."
-    list.newNote(text);
-    if (list.notes[0].text === text) {
-        console.log('notes saved')
+    var text0 = "Javascript is a wonderful language and the easiest to learn and ;everybody loves it."
+    list.newNote(text0);
+    if (list.notes[0].text === text0) {
+        console.log('notes saved');
     } else {
-        throw new Error('note not saved')
+        throw new Error('note not saved');
+    }
+
+    var text1 = "Ruby's also cool.";
+    list.newNote(text1);
+    var text2 = "I love CSS.";
+    list.newNote(text2);
+    if (list.allNotes().length === 3) {
+        console.log('correct number of notes saved');
+    } else {
+        throw new Error('wrong number of notes saved - ' + list.notes.length);
     }
 }
 
