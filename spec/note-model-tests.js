4438ffad98eaa583da624f5ee8b924513f99f7a1
Tomy8s <tomy8s@hotmail.com>
2016-11-07 12:36:59 +0000

    Added tests for text and abb. length

--- spec/note-model-tests.js
+++ spec/note-model-tests.js
@@ -1,8 +1,16 @@
-function testForNoteType() {
+function testsForNote() {
     var note = new Note();
-    if (typeof note !== Note) {
-        throw new Error('note is not a Note');
+    var text = "Javascript is a wonderful language and the easiest to learn and ;everybody loves it.";
+    note.text = text;
+    
+    if (note.text !== text) {
+        throw new Error('note does not contain text');
+    }
+
+    if (note.abbreviation.length > 20) {
+        throw new Error('abbreviation is too long! Max 20 characters.');
     }
 };
 
-testForNoteType();
+
+testsForNote();
