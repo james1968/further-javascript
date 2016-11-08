8175770e6571bd1f5fa968e2536057a2976e478d
Amaal Ali <amq.ali@gmail.com>
2016-11-08 14:10:36 +0000

    Tests fors HTML summary of notes in list.

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,18 +1,27 @@
 function isTextCorrect() {
 
 
-note = new Note('Test text entry');
-list = new NoteList();
-list.addNote(note);
-note1 = new Note('second test');
-list.addNote(note1);
+    var list = new NoteList();
+    var note = new Note('Test text entry for the first note');
+    var note1 = new Note('second test for the second note');
+    var noteListView = new NoteListView();
 
- if (note.printText() === 'Test text entry', 'second test') {
-   console.log('Test passed');
- } else {
-   console.log('Test failed');
- }
+    list.addNote(note);
+    list.addNote(note1);
 
+    if (note.printText() === 'Test text entry', 'second test') {
+        console.log('Test passed');
+    } else {
+        console.log('Test failed');
+    }
+
+    var htmlstring = '<ul><li><div>Test text entry for </div></li><li><div>second test for the </div></li></ul>'
+
+    if (noteListView.returnhtml(list) === htmlstring) {
+        console.log('Note List displayed');
+    } else {
+        console.log('Notes list not displayed');
+    }
 
 
 }
