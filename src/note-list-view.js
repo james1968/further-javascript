c838d95fc290098f1df056e7345fa2ef790b07e8
James <bakerje@mac.com>
2016-11-08 19:05:50 +0000

    List details added

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,14 +1,18 @@
 function NoteListView() {
 
+NoteListView.prototype.returnhtml = function(notesList) {
+  var array = notesList.allNotes();
+  var list = document.createElement('ul');
 
+  for(var i = 0; i < array.length;i++) {
 
-NoteListView.prototype.returnhtml = function (list) {
-
-
-    for(i=0;i<list.allNotes().length;i++) {
-
-    console.log('<ul><li><div>'+list.allNotes()('</div></li><li><div>')+'</div></li></ul>');
-
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(array[i].printText()));
+    list.appendChild(item);
     }
+    console.log(list);
+  return list;
+
 };
+
 }
