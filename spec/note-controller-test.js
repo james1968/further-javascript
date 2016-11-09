0eb561704d91da1d4380fbd1689855b5bbdc5a17
Tomy8s <tomy8s@hotmail.com>
2016-11-09 16:29:06 +0000

    passes test for displaying list

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -2,8 +2,8 @@ function noteControllerTest() {
 
   var noteListView = new NoteListView();
   var noteController = new NoteController(noteListView.returnhtml(list));
-  var result = '<ul><li><div>Test text entry for the first note</div></li><li><div>second test for the second note</div></li></ul>';
-  if (document.getElementById('app') == result){
+  var result = '<li><div>Test text entry for the first note</div></li><li><div>second test for the second note</div></li>';
+  if (document.getElementById('app').lastChild.innerHTML == result){
     console.log('Note List displayed');
   } else {
     throw new Error('Notes list not displayed');
